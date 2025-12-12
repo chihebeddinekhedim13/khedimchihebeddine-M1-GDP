5b2053171a56c9584f8ff3e35e72aa7c80c4ad1f
hadjerbh <joujoubenhsrradj@gmail.com>
2025-12-12 23:02:02 +0100

    Update Chiheb.py

--- Chiheb.py
+++ Chiheb.py
@@ -4,4 +4,14 @@
 # boukhobza ahmed ramy
 # bendada firas walid
 # belkhir manel
+# 5) Ajouter colonne "Catégorie GC"
+def categ(gc):
+          if gc > 55:
+            return "Riche"
+          elif 45 <= gc <= 55:
+            return "Moyen"
+          else:
+            return "Faible"
+df["Ctégorie_GC"]=df["Pourcentage_GC"].apply(categ)
+print(df)
 
